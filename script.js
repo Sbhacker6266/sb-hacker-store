9703884753ea102c0cc2832a4ff6dbdb94957640
SBHACKER6266 <sbhacker6266@gmail.com>
2025-04-25 18:30:16 +0530

    Added Add to Cart and Cart Display System

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // You can add cart logic here later
 console.log("SB HACKER STORE ready");
+function addToCart(productName) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push(productName);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert(productName + ' added to cart!');
+  }
